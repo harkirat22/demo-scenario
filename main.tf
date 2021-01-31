cf3ebcf7ca3fc51f4b6dde5e04b33b381e4cf57c
harkirat22 <harkiratbhardwaj@gmail.com>
2021-02-01 01:25:19 +1100

    changes

--- main.tf
+++ main.tf
@@ -334,7 +334,7 @@ resource "aws_instance" "web-app-instance" {
   tenancy = "default"
 
   provisioner "file" {
-    source      = "./demo/demo-website/app.zip"
+    source      = "./assets/ssrf_app/app.zip"
     destination = "/home/ubuntu/app.zip"
     connection {
       type        = "ssh"
